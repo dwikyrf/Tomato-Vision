9bd782080d9269075721ae6f66d6613fbd9bc172
Dwiky RF <dwikyrf@gmail.com>
2025-11-19 07:37:25 +0700

    Fix: Wrap carousel logic in main.js to prevent crash on camera page

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -99,11 +99,13 @@
   let heroCarouselIndicators = select("#hero-carousel-indicators")
   let heroCarouselItems = select('#heroCarousel .carousel-item', true)
 
-  heroCarouselItems.forEach((item, index) => {
-    (index === 0) ?
-    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
-      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
-  });
+ if (heroCarouselIndicators && heroCarouselItems.length > 0) { 
+    heroCarouselItems.forEach((item, index) => { 
+      (index === 0) ?
+        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+    });
+  }
 
   /**
    * Porfolio isotope and filter
